6fe70b3d96b6b6c490bce3e7794e370d8018b5c8
AlissonOliveira-Jar <alisson.dev.py@gmail.com>
2025-03-25 18:22:43 -0300

    Criação do projeto Spring juntamente com Docker e PostgreSQL

---
+++ src/main/java/estudo/tedw/agregadorinvestimentos/AgregadorinvestimentosApplication.java
@@ -0,0 +1,13 @@
+package estudo.tedw.agregadorinvestimentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgregadorinvestimentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgregadorinvestimentosApplication.class, args);
+	}
+
+}
