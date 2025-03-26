de8db1d95ad9a2d3c96ee049b61848769ca307ee
AlissonOliveira-Jar <alisson.dev.py@gmail.com>
2025-03-26 14:46:04 -0300

    feature: listar usuários

--- src/main/java/estudo/tedw/agregadorinvestimentos/controller/UserController.java
+++ src/main/java/estudo/tedw/agregadorinvestimentos/controller/UserController.java
@@ -6,12 +6,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/users/")
 public class UserController {
 
-    private UserService userService;
+    private final UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
@@ -27,5 +28,11 @@ public class UserController {
     public ResponseEntity<User> getUserById(@PathVariable String userID) {
         var user = userService.getUserById(userID);
         return user.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> listUsers() {
+        var users = userService.listUsers();
+        return ResponseEntity.ok(users);
     }
 }
--- src/main/java/estudo/tedw/agregadorinvestimentos/service/UserService.java
+++ src/main/java/estudo/tedw/agregadorinvestimentos/service/UserService.java
@@ -6,6 +6,7 @@ import estudo.tedw.agregadorinvestimentos.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -33,5 +34,9 @@ public class UserService {
 
     public Optional<User> getUserById(String userID) {
         return userRepository.findById(UUID.fromString(userID));
+    }
+
+    public List<User> listUsers() {
+        return userRepository.findAll();
     }
 }
